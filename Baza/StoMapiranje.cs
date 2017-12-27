cbd389e09f34fa437ac1c596868060f33afbc693
Nikolino95 <majmundevet@gmail.com>
2017-12-27 05:50:35 +0100

    baza
    b

---
+++ Baza/StoMapiranje.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+namespace OOProjektovanje_lab5.Mapiranja
+{
+    class StoMapiranje:ClassMap<Entiteti.Sto>
+    {
+        public StoMapiranje()
+        {
+            Table("STO");
+            Id(x => x.id, "IDSTO").GeneratedBy.TriggerIdentity();
+            Map(x => x.Naziv, "NAZIV");
+            Map(x => x.minUlog, "MINULOG");
+            Map(x => x.maxBrIgraca, "MAXBRIGRACA");
+            Map(x => x.trenutniBrIgraca, "TRENUTNIBRIGRACA");
+            HasMany(x => x.igraci).KeyColumn("STO").Inverse().Cascade.All();
+        }
+    }
+}
